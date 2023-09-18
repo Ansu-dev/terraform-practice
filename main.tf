3e5cc2541e96b0dcde868c85b6875864aab5c850
Ansu-dev <ansu1007@naver.com>
2023-09-18 16:14:02 +0900

    feat: data source 활용

--- main.tf
+++ main.tf
@@ -11,6 +11,7 @@ provider "aws" {
   region = "ap-northeast-2"
 }
 
+
 resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 
@@ -28,11 +29,17 @@ resource "aws_subnet" "public_subnet_1" {
   }
 }
 
-resource "aws_subnet" "private_subnet_1" {
+/* resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.default.id
   cidr_block = "10.0.100.0/24"
 
   tags = {
     Name = "fastcampus_private_subnet_1"
   }
+} */
+
+# resource의 네임과 설정은 로컬에서만 판별하기 때문에
+# datasource를 이용해서 클라우드 콘솔에 있는 id와 비교 할수 있음
+data "aws_subnet" "private_subnet_1" {
+  id = "subnet-055f04f6c48187fe4"
 }
